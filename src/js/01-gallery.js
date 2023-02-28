96721a3103ffe52d050e8940b02f5012ce3b9078
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-02-28 02:47:35 +0200

    add 1 task

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const imgContainer = galleryItems.reduce((acum, item) => {
+    return (acum += ` <div class = "gallery__item"> 
+    <a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+  </div>`);
+}, '');
+gallery.insertAdjacentHTML('beforeend', imgContainer);
+
+const ligthBox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250
+})
+console.log(galleryItems);
