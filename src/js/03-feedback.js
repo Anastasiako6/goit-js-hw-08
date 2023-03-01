abc86099320d31b42b823e6492f429f0e04e5519
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-03-02 00:29:42 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,7 +6,8 @@ const message = document.querySelector('.feedback-form textarea');
 const LOCAL_KEY = 'feedback-form-state';
 
 form.addEventListener('input', throttle(onInputData, 500));
-form.addEventListener('submit', onFormSubmit)
+form.addEventListener('submit', onFormSubmit);
+
 
 function onInputData (e) {
     infSave = { email: email.value, message: message.value };
@@ -27,40 +28,10 @@ function onFormSubmit(e) {
 
 
 function reloadPage() {
-    const infSave = localStorage.getItem(LOCAL_KEY);
-  if (infSave) {
-const { email, message } = JSON.parse(formData);
-    email.value = infSave.email || '';
-    message.value = infSave.message || '';
+  const formData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+  if (formData) {
+    email.value = formData.email;
+    message.value = formData.message;
   }
 }
 reloadPage();
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-//   console.log(getFormData());
-//   refs.email.value = '';
-//   refs.message.value = '';
-//   localStorage.removeItem(STORAGE_INPUT_KEY);
-// }
-
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// function onInput() {
-//   localStorage.setItem(STORAGE_INPUT_KEY, JSON.stringify(getFormData()));
-// }
-
-// const updateStorage = throttle(onInput, 500);
-
-// refs.form.addEventListener('input', updateStorage);
-
-// function innitData() {
-//   const formData = localStorage.getItem(STORAGE_INPUT_KEY);
-//   if (formData) {
-//     const { email, message } = JSON.parse(formData);
-//     refs.email.value = email;
-//     refs.message.value = message;
-//   }
-// }
-
-// innitData();
